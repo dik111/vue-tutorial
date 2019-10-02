cd2189b6f48dcfcc48be882026e81982f6ea501f
YuweiZhan <winzhanyuwei@163.com>
2019-10-02 21:29:28 +0800

    11.Vue-指令v-if

--- vue-playlist/app.js
+++ vue-playlist/app.js
@@ -2,19 +2,15 @@
 new Vue({
     el:"#vue-app",
     data:{
-        changeColor:false,
-        changeLength:false
+        error:false,
+        success:false
+
     },
     methods:{
 
     },
     computed:{
-            compClass:function () {
-                return {
-                    changeColor: this.changeColor,
-                    changeLength: this.changeLength
-                }
-            }
+
 
     }
 });
