35cc4d3d6b7d8499927910d5564156e25279a0bd
YuweiZhan <winzhanyuwei@163.com>
2019-10-02 18:56:23 +0800

    10.Vue-动态绑定CSS样式

--- vue-playlist/app.js
+++ vue-playlist/app.js
@@ -2,24 +2,19 @@
 new Vue({
     el:"#vue-app",
     data:{
-        a:0,
-        b:0,
-        age:20
+        changeColor:false,
+        changeLength:false
     },
     methods:{
-        // addToA:function () {
-        //     return this.a + this.age;
-        // },
-        // addToB:function () {
-        //     return this.b + this.age;
-        // }
+
     },
     computed:{
-        addToA:function () {
-            return this.a + this.age;
-        },
-        addToB:function () {
-            return this.b + this.age;
-        }
+            compClass:function () {
+                return {
+                    changeColor: this.changeColor,
+                    changeLength: this.changeLength
+                }
+            }
+
     }
 });
