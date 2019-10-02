52508f942119784d2f7f8f523ca402a17b79f8bb
YuweiZhan <winzhanyuwei@163.com>
2019-10-02 23:44:52 +0800

    13.Vue-实战DEMO

--- vue-playlist/app.js
+++ vue-playlist/app.js
@@ -2,17 +2,22 @@
 new Vue({
     el:"#vue-app",
     data:{
-       characters:["Mario","Luffy","Yoshi"],
-        users:[
-            {name:"Henry",age:30},
-            {name:"Bucky",age:25},
-            {name:"Emily",age:18}
-
-        ]
+        health:100,
+        ended:false
 
     },
     methods:{
+        punch:function () {
+            this.health -=10;
 
+            if (this.health <=0 ){
+                this.ended=true;
+            }
+        },
+        restart:function () {
+            this.health =100;
+            this.ended=false;
+        }
     },
     computed:{
 
