d70455ee33e64415c4d39351293ac206ee192500
YuweiZhan <winzhanyuwei@163.com>
2019-10-02 16:55:53 +0800

    8.Vue-双向数据绑定

--- vue-playlist/app.js
+++ vue-playlist/app.js
@@ -2,13 +2,17 @@
 new Vue({
     el:"#vue-app",
     data:{
+        name:"",
+        age:""
     },
     methods:{
        logName:function () {
-           console.log("你正在输入名字!")
+           // console.log("你正在输入名字!")
+           this.name = this.$refs.name.value;
        },
         logAge:function () {
-            console.log("你正在输入年龄!")
+            // console.log("你正在输入年龄!")
+            this.age = this.$refs.age.value;
         }
 
     }
