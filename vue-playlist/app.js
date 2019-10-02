9b0d24914bb6782ec15f48eaca0999b818f489d7
YuweiZhan <winzhanyuwei@163.com>
2019-10-02 15:56:46 +0800

    5.Vue-事件(点击:双击:鼠标事件)

--- vue-playlist/app.js
+++ vue-playlist/app.js
@@ -2,11 +2,21 @@
 new Vue({
     el:"#vue-app",
     data:{
-        name:"mrwu"
+        age:30,
+        x:0,
+        y:0
     },
     methods:{
-        greet:function (time) {
-            return 'Good '+time +' '+this.name+'!';
+        add:function (inc) {
+            this.age+=inc;
+        },
+        subtract:function (dec) {
+            this.age-=dec;
+        },
+        updateXY:function (event) {
+            // console.log(event);
+            this.x=event.offsetX;
+            this.y=event.offsetY;
         }
     }
 });
