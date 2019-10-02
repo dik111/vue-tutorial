10ce30eb901889629a4a780f0904e3e210a30155
YuweiZhan <winzhanyuwei@163.com>
2019-10-02 12:50:12 +0800

    3.Vue-数据&方法

--- vue-playlist/app.js
+++ vue-playlist/app.js
@@ -3,5 +3,10 @@ new Vue({
     el:"#vue-app",
     data:{
         name:"mrwu"
+    },
+    methods:{
+        greet:function (time) {
+            return 'Good '+time +' '+this.name+'!';
+        }
     }
 });
