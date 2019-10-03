83d92bc8f9fe694cbcaafa419a4d9694321d9bf3
YuweiZhan <winzhanyuwei@163.com>
2019-10-03 11:57:25 +0800

    16.Vue-搭建脚手架

---
+++ vue-playlist2/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    Hello Vue.js
+    <h1>{{title}}</h1>
+  </div>
+</template>
+
+<script>
+// import HelloWorld from './components/HelloWorld'
+
+export default {
+  name: 'App',
+  data(){
+      return{
+          title:"这个是我的第一个Vue脚手架项目！"
+      }
+  }
+}
+</script>
+
+<style>
+
+</style>
